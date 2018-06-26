e84ce50d37be3d95f93e35833594b367e4efe830
Roman Malyugin <roman.malyugin@altabel.com>
2018-06-26 11:13:39 +0200

    initial for manager

---
+++ org.openhab.binding.openhabprometheusmetrics/src/main/java/org/openhab/binding/openhabprometheusmetrics/internal/OpenHABPrometheusMetricsThingManager.java
@@ -0,0 +1,32 @@
+package org.openhab.binding.openhabprometheusmetrics.internal;
+
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.internal.ThingTracker;
+
+public class OpenHABPrometheusMetricsThingManager implements ThingTracker {
+
+    @Override
+    public void thingAdded(Thing thing, ThingTrackerEvent thingTrackerEvent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void thingRemoving(Thing thing, ThingTrackerEvent thingTrackerEvent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void thingRemoved(Thing thing, ThingTrackerEvent thingTrackerEvent) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void thingUpdated(Thing thing, ThingTrackerEvent thingTrackerEvent) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
