68becd1331e490321f606f9e18bdb7151aaac8ad
ronx <ronx@ronx-laptop>
2018-07-05 12:21:23 +0200

    added testing during package phase

---
+++ org.eclipse.io.prometheusmetrics/src/test/java/org/eclipse/io/prometheusmetrics/DummyTest.java
@@ -0,0 +1,14 @@
+package org.eclipse.io.prometheusmetrics;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() throws Exception {
+        assertEquals(1, 1);
+    }
+
+}
