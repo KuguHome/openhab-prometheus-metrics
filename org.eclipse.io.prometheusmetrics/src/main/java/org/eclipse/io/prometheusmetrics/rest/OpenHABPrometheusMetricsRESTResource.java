638f2bc24e1b5a08a397bf627e011a30c30417e3
Roman Malyugin <roman.malyugin@altabel.com>
2018-07-11 18:07:14 +0200

    trying to implement custom appender

--- org.eclipse.io.prometheusmetrics/src/main/java/org/eclipse/io/prometheusmetrics/rest/OpenHABPrometheusMetricsRESTResource.java
+++ org.eclipse.io.prometheusmetrics/src/main/java/org/eclipse/io/prometheusmetrics/rest/OpenHABPrometheusMetricsRESTResource.java
@@ -17,6 +17,7 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.LogManager;
 import java.util.stream.Stream;
 
 import javax.annotation.security.RolesAllowed;
@@ -29,6 +30,10 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.smarthome.config.discovery.DiscoveryResult;
 import org.eclipse.smarthome.config.discovery.inbox.Inbox;
@@ -51,7 +56,6 @@ import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.prometheus.client.CollectorRegistry;
@@ -163,6 +167,15 @@ public class OpenHABPrometheusMetricsRESTResource /* extends EventBridge */
 
     @Activate
     protected void activate() {
+        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        Configuration config = ctx.getConfiguration();
+        ConsoleAppender consoleAppender = ConsoleAppender
+                .createDefaultAppenderForLayout(PatternLayout.createDefaultLayout());
+        consoleAppender.start(); // this is optional
+        config.addAppender(consoleAppender); // this is optional
+        ctx.getRootLogger().addAppender(consoleAppender);
+        ctx.updateLoggers();
+
         try {
             httpService.registerResources(METRICS_ALIAS, "web", null);
             logger.info("Started Metrics at " + METRICS_ALIAS);
